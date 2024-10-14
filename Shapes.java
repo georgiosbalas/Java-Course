3dcff963900aa8972850844c31db14b82b0a0aae
georgiosbalas <164395745+georgiosbalas@users.noreply.github.com>
2024-10-14 19:09:58 +0300

    Add files via upload

---
+++ Shapes.java
@@ -0,0 +1,16 @@
+
+public class Shapes {
+
+    public static void main(String[] args) {
+        // Σχήμα 1: Τρίγωνα
+        System.out.println("     *                  *");
+        System.out.println("    ***                ***");
+        System.out.println("   *****              *****");
+        System.out.println("  *******            *******");
+        System.out.println(" *********          *********");
+        System.out.println("    ***              *******");
+        System.out.println("    ***               *****");
+        System.out.println(" *********             ***");
+        System.out.println("                        *");
+    }
+}
